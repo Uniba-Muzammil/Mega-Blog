93607e7696485a2fb2af075575af96e79e3de14a
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-03-12 15:09:50 +0500

    update

---
+++ src/appwrite/auth.js
@@ -0,0 +1,38 @@
+import conf from "../conf.js"
+import { Client, Account, ID } from "appwrite";
+export class AuthService{
+client=new Client();
+account;
+constructor(){
+    this.client
+    .setEndpoint()
+    .setProject()
+    this.account=new Account(this.client)
+}
+async login({email,password,name}){
+    try{
+        const userAccount=await this.account.create(ID.unique() ,email,password,name)
+        if(userAccount){
+           return this.login({email,password})
+        }
+        else{
+            return userAccount
+        }
+    }
+    catch(error){
+       throw error
+    }
+}
+async login({email,password}){
+    try{
+      return await this.account.createEmailPasswordSession(email,password)  
+    }
+    catch(error){
+        throw error
+    }
+}
+}
+
+const authService= new AuthService();
+
+export default authService;
